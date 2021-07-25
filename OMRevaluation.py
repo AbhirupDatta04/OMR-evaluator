e5850334386c162b5c54872dc7ffe48b620a47ab
Abhirup <abhirupdatta4@gmail.com>
2021-07-25 18:37:06 +0530

    Final Commit Grading done

--- OMRevaluation.py
+++ OMRevaluation.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import imutils
 import argparse
+from argparse import ArgumentParse
 from imutils.perspective import four_point_transform
 from imutils import contours
 
@@ -58,4 +59,45 @@ for c in cnts:
     if w >= 20 and h >= 20 and ar >= 0.9 and ar <= 1.1:
         questionCnts.append(c)
         
+questionCnts = contours.sort_contours(questionCnts, method = "top-to-bottom")[0]
+correct = 0
+
+
+for(q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
+   
+    cnts = contours.sort_contours(questionCnts[i:i+5])[0]
+    bubbled = None
+ 
+
+    for (j, c) in enumerate(cnts):
         
+        mask = np.zeros(thresh.shape, dtype = "uint8") #mask that reveals only the current "bubble" for the question
+        cv.drawContours(mask, [c], -1, 255, -1)
+
+     
+        mask = cv.bitwise_and(thresh, thresh, mask = mask)# #mask application to the threshold image
+        total = cv.countNonZero(mask)     # count the number of non-zero pixels in the bubble area
+
+    
+        if bubbled is None or total > bubbled[0]:
+            bubbled = (total, j)
+
+
+    color = (0, 0, 255)
+    k = ANSWER_KEY[q]  #looking up for the answer in the answer key and initialize the contour color and the index of the correct answer
+
+    #To see if the bubbled answer is correct
+    if k == bubbled[1]:
+        color = (0, 255, 0)
+        correct += 1
+        
+        #to draw the outlineof the correct answer on the test
+    cv.drawContours(paper, [cnts[k]], -1, color, 3)
+
+score = (correct / 5.0) * 100
+print ("[INFO] score: {:.2f}%".format(score))
+cv.putText(paper, "{:.2f}%".format(score), (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
+cv.imshow("Original", image)
+cv.imshow("Exam", paper)
+cv.waitKey(0)
+       
